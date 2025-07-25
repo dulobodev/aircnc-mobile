bd64fed2833f07191308469fa4da17f2d786f0a6
EDUARDO COSTA LOBO <eduardo.clobo@senacsp.edu.br>
2025-07-24 22:06:14 -0300

    App mobile Aircnc

--- App.tsx
+++ App.tsx
@@ -1,20 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+import logo from "./src/assets/logo.png";
+
+import api from "./src/services/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {}, []);
+
+  async function handleSubmit() {
+    console.log(email,techs)
+    const response = await api.post("/session", {email});
+    console.log("Resposta da API:", response.data);
+
+    const { _id } = response.data;
+    await AsyncStorage.setItem("user", _id);
+    await AsyncStorage.setItem("techs", techs);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Image source={logo}></Image>
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Seu e-mail..."
+          placeholderTextColor="#0007"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>TECNOLOGIAS</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Tecnologias de interesse"
+          placeholderTextColor="#0007"
+          value={techs}
+          onChangeText={setTechs}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          // onPress={() => {}}
+          onPress={handleSubmit}
+        >
+          <Text style={styles.buttonText}>Encontrar Spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    alignSelf: "stretch",
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+  label: {
+    alignSelf: "stretch",
+    paddingHorizontal: 0,
+    paddingTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#0003",
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: "#444",
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2,
+  },
+  button: {
+    height: 42,
+    backgroundColor: "#f05a5b",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
